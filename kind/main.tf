2350b10ca0e2d29f804f84187f4918b1669027d6
GersonRS <gersonrodriguessantos8@gmail.com>
2026-01-25 18:32:55 -0300

    feat: adjust kubernetes service version

--- kind/main.tf
+++ kind/main.tf
@@ -17,7 +17,7 @@ module "traefik" {
   dependency_ids = var.dependency_ids
 }
 
-data "kubernetes_service" "traefik" {
+data "kubernetes_service_v1" "traefik" {
   metadata {
     name      = replace(format("%s%s", local.helm_values.0.traefik.fullnameOverride, module.traefik.id), module.traefik.id, "")
     namespace = "traefik"
